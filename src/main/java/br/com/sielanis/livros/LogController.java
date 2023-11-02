8448d48b9fefeb75e3d4ba07d4ddd71cff37189f
Raazak <rlalves4880@gmail.com>
2023-11-02 09:01:03 -0300

    api e metodo de log

--- src/main/java/br/com/sielanis/livros/LogController.java
+++ src/main/java/br/com/sielanis/livros/LogController.java
@@ -1,5 +1,9 @@
 package br.com.sielanis.livros;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +18,17 @@ public class LogController {
 	@RequestMapping("/inserirlog")
 	public String InserirLog(@RequestParam(value="create_time", defaultValue = "2000-01-01") String create_time) {
 		logRepositorio = new LogRepositorio();
-		logRepositorio.InserirLog(create_time);
+		
+        SimpleDateFormat formata = new SimpleDateFormat("yyyy-MM-dd");
+        Date data;
+        
+		try {
+			data = formata.parse(create_time);
+			logRepositorio.InserirLog(data);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return "olha o log";
 	}
--- src/main/java/br/com/sielanis/livros/repositorio/LogRepositorio.java
+++ src/main/java/br/com/sielanis/livros/repositorio/LogRepositorio.java
@@ -4,26 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LogRepositorio extends BaseRepositorio {
 	
-	public void InserirLog(String create_time) {
+	public void InserirLog(Date data) {
 	
-        SimpleDateFormat formata = new SimpleDateFormat("yyyy-MM-dd");
-        Date data = formata.parse(create_time);
         java.sql.Date sqlDate = new java.sql.Date(data.getTime());
-		
+			
 		try (Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA)) {
             String sql = "INSERT INTO Log (create_time) VALUES (?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setDate(1, (java.sql.Date) sqlDate);
-                
+	                
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }		
+        }	
 	}
 }
